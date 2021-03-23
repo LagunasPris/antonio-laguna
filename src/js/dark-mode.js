375dce692ea6277186280e06b020e1eae86a6c97
Antonio Laguna <sombragriselros@gmail.com>
2021-03-23 22:13:45 +0100

    Adding Toggler

---
+++ src/js/dark-mode.js
@@ -0,0 +1,28 @@
+(() => {
+  const Themes = {
+    Dark: 'dark',
+    Light: 'light'
+  };
+  const ModeLabel = {
+    dark: 'luminoso',
+    light: 'oscuro'
+  };
+  const Key = 'theme';
+
+  const root = document.documentElement;
+  let currentTheme = root.dataset.theme;
+  const button = document.querySelector('.color-scheme-toggler');
+  const getLabel = () => `Cambiar a modo ${ModeLabel[currentTheme]}`;
+
+  const onButtonClick = () => {
+    const newTheme = currentTheme === Themes.Dark ? Themes.Light : Themes.Dark;
+
+    currentTheme = newTheme;
+    root.dataset.theme = newTheme;
+    localStorage.setItem(Key, newTheme);
+    button.setAttribute('aria-label', getLabel());
+  };
+
+  button.setAttribute('aria-label', getLabel());
+  button.addEventListener('click', onButtonClick);
+})();
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './js/dark-mode';
 import './js/lazy-loading';
 import './js/easter-egg';
 import './js/toc';
