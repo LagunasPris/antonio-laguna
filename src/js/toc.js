f08c9413aea8adcbeb84889e989cbc8f860ce833
Antonio Laguna <sombragriselros@gmail.com>
2021-03-25 21:04:22 +0100

    Removing unneded commented code

--- src/js/toc.js
+++ src/js/toc.js
@@ -67,35 +67,3 @@ import watchResizers from './resizers';
   onResize();
   handleScroll();
 })();
-
-/*
-
-window.addEventListener("load", () => {
- // Retrieve all help sections
- const sections = Array.from(document.querySelectorAll("section[id]"));
-
- // Once a scrolling event is detected, iterate all elements
- // whose visibility changed and highlight their navigation entry
- const scrollHandler = entries =>
-  entries.forEach(entry => {
-   const section = entry.target;
-   const sectionId = section.id;
-   const sectionLink = document.querySelector(`a[href="#${sectionId}"]`);
-
-   if (entry.intersectionRatio > 0) {
-    section.classList.add("visible");
-    sectionLink.classList.add("visible");
-   } else {
-    section.classList.remove("visible");
-    sectionLink.classList.remove("visible");
-   }
-  });
-
- // Creates a new scroll observer
- const observer = new IntersectionObserver(scrollHandler);
-
- //noinspection JSCheckFunctionSignatures
- sections.forEach(section => observer.observe(section));
-});
-
- */
