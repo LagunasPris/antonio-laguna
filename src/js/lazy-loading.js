20e930c7bc35b51d5fa214c81693b4baf432d192
Antonio Laguna <sombragriselros@gmail.com>
2021-04-06 21:41:00 +0200

    Fixing Safari

--- src/js/lazy-loading.js
+++ src/js/lazy-loading.js
@@ -1,16 +1,18 @@
 import lozad from 'lozad';
 
 function lazyload() {
-  const lazyEls = Array.from(document.querySelectorAll('.lazy'));
+  const lazyEls = document.querySelectorAll('.lazy');
 
   if (!lazyEls.length) {
     return;
   }
 
-  lazyEls.forEach(el => el.classList.remove('lazy'));
+  const lazyArray = Array.from(lazyEls);
+
+  lazyArray.forEach(el => el.classList.remove('lazy'));
 
   if ('loading' in HTMLImageElement.prototype) {
-    lazyEls.forEach(lazyEl => {
+    lazyArray.forEach(lazyEl => {
       lazyEl.setAttribute('src', lazyEl.getAttribute('data-src'));
     });
   } else {
